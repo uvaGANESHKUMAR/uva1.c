4b3b97cda3f6bdbe8633af76eeced05c61e49a72
uvaGANESHKUMAR <35688981+uvaGANESHKUMAR@users.noreply.github.com>
2018-01-31 16:03:07 +0530

    Create uva1.c

---
+++ uva1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<coino.h>
+void main()
+{
+char b;
+printf("enter the letter");
+sacnf("%c",&b);
+if(b==a,b==e,b==i,b==o,b==u)
+printf("it is a vowel");
+else
+printf("it is a constant");
+}
