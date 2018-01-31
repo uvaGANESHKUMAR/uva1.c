45c804cc8625a74ff877df92abed1d527fb197f1
uvaGANESHKUMAR <35688981+uvaGANESHKUMAR@users.noreply.github.com>
2018-01-31 16:39:24 +0530

    uva1.c

--- uva1.c
+++ uva1.c
@@ -1,12 +1,24 @@
-#include<stdio.h>
-#include<coino.h>
-void main()
+include <stdio.h>
+#include <conio.h>
+int main()
 {
-char b;
-printf("enter the letter");
-sacnf("%c",&b);
-if(b==a,b==e,b==i,b==o,b==u)
-printf("it is a vowel");
-else
-printf("it is a constant");
+    char c;
+    int isLowercaseVowel, isUppercaseVowel;
+
+    printf("Enter an alphabet: ");
+    scanf("%c",&c);
+
+    // evaluates to 1 (true) if c is a lowercase vowel
+    isLowercaseVowel = (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u');
+
+    // evaluates to 1 (true) if c is an uppercase vowel
+    isUppercaseVowel = (c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U');
+
+    // evaluates to 1 (true) if either isLowercaseVowel or isUppercaseVowel is true
+    if (isLowercaseVowel || isUppercaseVowel)
+        printf("%c is a vowel.", c);
+    else
+        printf("%c is a consonant.", c);
+    getch();
+    return 0;
 }
